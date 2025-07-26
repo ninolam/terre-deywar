8a112875aa2de01e0fad4914a96f6c5d0cafa961
Lamine-BOUMAZA <lamsbmz92@gmail.com>
2025-07-26 18:06:42 +0200

    config page footer html ok

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Link from "next/link";
+
+export const Footer = () => {
+  return (
+    <footer className="Footer">
+
+    <section>
+
+      <div>
+        <h3>Liens Utiles</h3>
+        <Link href="/home">À Propos</Link>
+        <Link href="/home">Nos Circuits</Link>
+        <Link href="/home">Témoignages Clients</Link>
+      </div>
+
+      <div>
+        <h3>À Propos</h3>
+        <Link href="/home">Contactez-nous</Link>
+        <Link href="/home">Blog Voyage</Link>
+        <Link href="/home">FAQ</Link>
+      </div>
+
+      <div>
+        <h3>Droits d'Auteur</h3>
+        <Link href="/home">Mentions légales</Link>
+        <Link href="/home">Politique de confidentialité</Link>
+        <Link href="/home">condition Générale d'Utilisation</Link>
+      </div>
+
+    </section>
+
+    {/* <img src="" alt="" /> */}
+
+    <section>
+      <div>
+        <p>© 2025 Terre d'Eywar. Tous droits réservés.</p>
+      </div>
+    </section>
+
+    </footer>
+  )
+}
