98cc05801d766bbddb87aeddd9446ec9238e6b72
Lamine-BOUMAZA <lamsbmz92@gmail.com>
2025-07-28 23:32:41 +0200

    footer comment PR ok

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,34 +1,27 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { footerBlocs } from "@/utils/data";
 
 const Footer = () => {
   return (
     <footer className="Footer">
       <section className="Footer__container">
         <div className="Footer__links">
-          <div className="Footer__links__bloc">
-            <h3>Liens Utiles</h3>
-            <Link href="/home">À Propos</Link>
-            <Link href="/home">Nos Circuits</Link>
-            <Link href="/home">Témoignages Clients</Link>
-          </div>
-          <div className="Footer__links__bloc">
-            <h3>À Propos</h3>
-            <Link href="/home">Contactez-nous</Link>
-            <Link href="/home">Blog Voyage</Link>
-            <Link href="/home">FAQ</Link>
-          </div>
-          <div className="Footer__links__bloc">
-            <h3>Droits d&apos;Auteur</h3>
-            <Link href="/home">Mentions légales</Link>
-            <Link href="/home">Politique de confidentialité</Link>
-            <Link href="/home">condition Générale d&apos;Utilisation</Link>
-          </div>
+          {footerBlocs.map((bloc) => (
+            <div className="Footer__links__bloc" key={bloc.title}>
+              <h3>{bloc.title}</h3>
+              {bloc.links.map((link) => (
+                <Link key={link.label} href={link.href}>
+                  {link.label}
+                </Link>
+              ))}
+            </div>
+          ))}
         </div>
         <div className="Footer__logo">
           <Link href="/">
-            <Image src="/logo.jpeg" alt="Logo" width={90} height={90} />
+            <Image src="/logo.jpeg" alt="Logo" width={120} height={120} />
           </Link>
         </div>
         <div className="Footer__copyright">
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,35 +1,29 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import Button from './Button';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Button from "./Button";
+import { headerLinks } from "@/utils/data";
 
 const Header = () => {
-  const links = [
-    {
-      label: 'Nos circuits',
-      href: '/circuits',
-    },
-    {
-      label: 'Notre histoire',
-      href: '/about',
-    },
-    {
-      label: 'Contactez-nous',
-      href: '/contact',
-    },
-  ];
-
   return (
     <header className="Header">
       <div className="Header__container">
         <Link href="/">
-          <Image className="Header__logo" src="/logo.jpeg" alt="Logo" width={90} height={90} />
+          <Image
+            className="Header__logo"
+            src="/logo.jpeg"
+            alt="Logo"
+            width={90}
+            height={90}
+          />
         </Link>
         <nav className="Header__nav">
           <ul className="Header__links">
-            {links.map((link) => (
+            {headerLinks.map((link) => (
               <li key={link.label}>
-                <Link className="Header__link" href={link.href}>{link.label}</Link>
+                <Link className="Header__link" href={link.href}>
+                  {link.label}
+                </Link>
               </li>
             ))}
           </ul>
