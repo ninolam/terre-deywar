c83df41bf4d212d938b072b1b7237da119af074a
ninolam <lamoureux.nino@gmail.com>
2025-08-06 19:44:44 +0200

    fix last details

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -89,16 +89,16 @@ export const circuits = [
   {
     title: 'TADRAT ROUGE',
     description: 'Touareg, music, bivouac',
-    image: '/images/section-circuits/image-circuit-1.jpg',
+    image: '/images/section-circuits/image-circuit-2.jpg',
   },
   {
     title: 'IHRIR, ESSENDILENE Bivouac',
     description: 'Touareg, music, bivouac',
-    image: '/images/section-circuits/image-circuit-2.png',
+    image: '/images/section-circuits/image-circuit-1.jpg',
   },
   {
     title: 'Tamanrasset',
     description: 'Touareg, music, bivouac',
-    image: '/images/section-circuits/image-circuit-3.jpeg',
+    image: '/images/section-circuits/image-circuit-3.jpg',
   },
 ];
